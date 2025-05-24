1031e68c9d8a19eeadd72973f65e35b32373731f
Barac Fabregat <barac.fabregat.gallegos@tuta.com>
2025-05-23 22:21:09 -0600

    chore: develop functionality into endpoints handlers

--- internal/handlers/encuesta_nps.go
+++ internal/handlers/encuesta_nps.go
@@ -3,7 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/calmestend/whatsapp-messaging-service/internal/models"
 	"github.com/go-playground/validator/v10"
@@ -20,6 +24,13 @@ type EncuestaNpsRequest struct {
 
 // Handle template "encuesta_nps" from the Whatsapp Business API from Meta
 func EncuestaNps(w http.ResponseWriter, r *http.Request) {
+	var token string = os.Getenv("WBA_TOKEN")
+	if token == "" {
+		fmt.Println(token)
+		http.Error(w, "Missing WBA_TOKEN in environment", http.StatusInternalServerError)
+		return
+	}
+
 	var payloadData EncuestaNpsRequest
 	err := json.NewDecoder(r.Body).Decode(&payloadData)
 	if err != nil {
@@ -37,8 +48,52 @@ func EncuestaNps(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	url := fmt.Sprintf("https://graph.facebook.com/v22.0/%s/messages", payloadData.From)
+	payloadMessage := fmt.Sprintf(`{
+	"messaging_product": "whatsapp",
+	"to": "%s",
+	"type": "template",
+	"template": {
+		"name": "encuesta_nps",
+		"language": { "code": "es" },
+		"components": [
+				{
+					"type": "body",
+					"parameters": [
+						{ "type": "text", "text": "%s"},
+						{ "type": "text", "text": "%s"},
+					]
+				}
+			]
+		}
+ }`, payloadData.To, payloadData.Address, payloadData.Date)
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(payloadMessage))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Request creation error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("HTTP request error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Reading response error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
 	w.Header().Set("Content-type", "application/json")
 	fmt.Fprintln(w, "Message sent")
-	json.NewEncoder(w).Encode(payloadData)
+	json.NewEncoder(w).Encode(string(respBody))
 }
--- internal/handlers/pagos_atrasados.go
+++ internal/handlers/pagos_atrasados.go
@@ -3,7 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/calmestend/whatsapp-messaging-service/internal/models"
 	"github.com/go-playground/validator/v10"
@@ -18,12 +22,20 @@ type PagosAtrasadosRequest struct {
 	Item        string `json:"item" validate:"required"`
 	Amount      string `json:"amount" validate:"required"`
 	Days        int    `json:"days" validate:"required,min=1"`
+	Condition   string `json:"condition" validate:"required"`
 	Payment_url string `json:"payment_url" validate:"required,http_url"`
 	Support_url string `json:"support_url" validate:"required,http_url"`
 }
 
 // Handle template "pagos_atrasados" from the Whatsapp Business API from Meta
 func PagosAtrasados(w http.ResponseWriter, r *http.Request) {
+	var token string = os.Getenv("WBA_TOKEN")
+	if token == "" {
+		fmt.Println(token)
+		http.Error(w, "Missing WBA_TOKEN in environment", http.StatusInternalServerError)
+		return
+	}
+
 	var payloadData PagosAtrasadosRequest
 	err := json.NewDecoder(r.Body).Decode(&payloadData)
 	if err != nil {
@@ -41,8 +53,54 @@ func PagosAtrasados(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	url := fmt.Sprintf("https://graph.facebook.com/v22.0/%s/messages", payloadData.From)
+	payloadMessage := fmt.Sprintf(`{
+	"messaging_product": "whatsapp",
+	"to": "%s",
+	"type": "template",
+	"template": {
+		"name": "pagos_atrasados",
+		"language": { "code": "es" },
+		"components": [
+				{
+					"type": "body",
+					"parameters": [
+						{ "type": "text", "text": "%s"},
+						{ "type": "text", "text": "%s"},
+						{ "type": "text", "text": "%d"},
+						{ "type": "text", "text": "%s"}
+					]
+				}
+			]
+		}
+ }`, payloadData.To, payloadData.Item, payloadData.Amount, payloadData.Days, payloadData.Condition)
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(payloadMessage))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Request creation error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("HTTP request error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Reading response error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
 	w.Header().Set("Content-type", "application/json")
 	fmt.Fprintln(w, "Message sent")
-	json.NewEncoder(w).Encode(payloadData)
+	json.NewEncoder(w).Encode(string(respBody))
 }
